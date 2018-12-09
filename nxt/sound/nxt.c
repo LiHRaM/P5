de6584b191efe3c25c536d832e2dd8989c42043c
Hilmar Gústafsson <hilmargustafs@gmail.com>
2018-12-09 01:28:13 +0100

    Progress on sound

--- nxt/sound/nxt.c
+++ nxt/sound/nxt.c
@@ -7,9 +7,13 @@
  * Author: 	Hilmar Gústafsson
  * Email: 	hgusta16@student.aau.dk
  */
+/* nxtOSEK */
 #include "kernel.h"
 #include "kernel_id.h"
 #include "ecrobot_interface.h"
+
+/* user files */
+#include "shared.h"
 
 /* nxtOSEK hooks */
 void ecrobot_device_initialize()
@@ -20,6 +24,13 @@ void ecrobot_device_initialize()
 void ecrobot_device_terminate()
 {
     ecrobot_term_bt_connection();
+}
+
+/**
+ * Fetch sound sensor value and compress it.
+ */
+U8 read_sound() {
+    return reduce_u16_to_u8(ecrobot_get_sound_sensor(NXT_PORT_S1));
 }
 
 /* nxtOSEK hook to be invoked from an ISR in category 2 */
@@ -34,27 +45,30 @@ void user_1ms_isr_type2(void) {}
 TASK(MainTask)
 {
     static U8 i;
-    static U16 rx_buf[BUF_SIZE]; // for a total of 256 bytes, which is the max allowed by ecrobot_send_bt
+    static RXBUF rx_buf;
 
-    for (i = 0; i < BUF_SIZE; i++)
-    {
-        rx_buf[i] = ecrobot_get_sound_sensor(NXT_PORT_S1);
-    }
+    // Repeat forever
+    while(1) {
+        for (i = 0; i < BUF_SIZE; i++)
+        {
+            rx_buf[i] = read_sound();
+            systick_wait_ms(1);
+        }
 
-    /* Lock until the state is BT_STREAM */
-    while (ecrobot_get_bt_status() != BT_STREAM)
-        ; // spin
+        /* Lock until the state is BT_STREAM */
+        while (ecrobot_get_bt_status() != BT_STREAM)
+            ; // spin
 
-    /* Continue sending the same packet until everything has been received. */
-    U8 sent = 0;
-    while (sent < 256)
-    {
-        sent += ecrobot_send_bt(rx_buf + sent, 0, (BUF_SIZE * 2) - sent);
+        /* Continue sending the same packet until everything has been received. */
+        U8 sent = 0;
+        while (sent < BUF_SIZE)
+        {
+            sent += ecrobot_send_bt(rx_buf + sent, 0, BUF_SIZE - sent);
 
-        /* Hopefully we won't lose more than a few ms sending the packet. */
+            /* Hopefully we won't lose more than a few ms sending the packet. */
+            systick_wait_ms(1);
+        }
         systick_wait_ms(1);
     }
-    systick_wait_ms(1);
-
     TerminateTask();
 }
